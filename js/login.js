a9f30288d83f2afa688dae4002277c503571a722
Olecram11 <marcelotejera11@gmail.com>
2021-08-19 17:47:30 -0300

    cambio de index a home

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
+function validar() {
+    var user = document.getElementById("inputEmail");
+    var pass = document.getElementById("inputPassword");
+    if ((user.value !== "") && (pass.value !== "")) {
+        window.location.href = "index.html";
+    } else {
+        alert("Debe completar los campos")
+    }
+}
+document.addEventListener("DOMContentLoaded", function (e) {
+   validar();
 });
