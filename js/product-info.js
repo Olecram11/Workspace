90b6714911cfe56e3370edbf218a60031ba59e58
Olecram11 <marcelotejera11@gmail.com>
2021-09-30 12:23:49 -0300

    commit

--- js/product-info.js
+++ js/product-info.js
@@ -11,14 +11,18 @@ function showProductImagesGallery(array){
 
     for(let i = 0; i < array.length; i++){
         let imageSrc = array[i];
+        if (i == 0) {
+            htmlContentToAppend = ` <div class="carousel-item active">
+        <img src="` + imageSrc + `" class="d-block w-100" alt="...">
+        </div>`
+        } else {
+            htmlContentToAppend += ` <div class="carousel-item">
+       <img src="` + imageSrc + `" class="d-block w-100" alt="...">
+       </div>`
+        }
 
-        htmlContentToAppend += `
-        <div class="col-lg-3 col-md-4 col-6">
-            <div class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
-            </div>
-        </div>
-        `
+      
+        
 
         document.getElementById("productInfoPicturesGallery").innerHTML = htmlContentToAppend;
     }
@@ -58,42 +62,20 @@ for(let i = 0; i < lista.length; i++){
     let posicion = lista[i];
 
         htmlContentToAppend += `
-        <div>
-        <p>${showProductsURL[posicion].name} </p>
-                
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${showProductsURL[posicion].imgSrc}" alt="Card image cap">
+        <div class="card-body">
+          <p class="card-text">${showProductsURL[posicion].name}</p>
+          <p class="card-text">${showProductsURL[posicion].description}</p>
+          <p class="card-text">Cantidad de vendidos = ${showProductsURL[posicion].soldCount}</p>
+          <p class="card-text">${showProductsURL[posicion].currency}</p>
+          <p class="card-text">${showProductsURL[posicion].cost}</p>
         </div>
+      </div>
         `
 }
 document.getElementById("productos-relacionados").innerHTML = htmlContentToAppend;
 }
-
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            product = resultObj.data;
-
-            let productNameHTML  = document.getElementById("productName");
-            let productDescriptionHTML = document.getElementById("productDescription");
-            let productSoldCountHTML = document.getElementById("productSoldCount");
-            let productPriceHTML = document.getElementById("productPrice");
-            let productCategoryHTML = document.getElementById("productCategory");
-
-            productNameHTML.innerHTML = product.name;
-            productDescriptionHTML.innerHTML = product.description;
-            productSoldCountHTML.innerHTML = product.soldCount;
-            productPriceHTML.innerHTML = product.currency + product.cost;
-
-            productCategoryHTML.innerHTML = product.category;
-
-            //Muestro las imagenes en forma de galería
-            showProductImagesGallery(product.images);
-        }
-    });
-});
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -104,16 +86,49 @@ document.addEventListener("DOMContentLoaded", function(e){
             showComment = resultObj.data
 
             showCommentlist(showComment);
-
-        getJSONData(PRODUCTS_URL).then(function(resultObj2){
-            if(resultObj2.status === "ok"){
-                showProductsURL = resultObj2.data
-
-                showRelatedProducts(product.relatedProducts)
-            }
-        })
-        
+           
     }
     });
+    getJSONData(PRODUCTS_URL).then(function(resultObj2){
+        if(resultObj2.status === "ok"){
+            showProductsURL = resultObj2.data
+            
+            getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+                if (resultObj.status === "ok")
+                {
+
+                    
+                    product = resultObj.data;
+        
+                    showRelatedProducts(product.relatedProducts)
+
+                    let productNameHTML  = document.getElementById("productName");
+                    let productDescriptionHTML = document.getElementById("productDescription");
+                    let productSoldCountHTML = document.getElementById("productSoldCount");
+                    let productPriceHTML = document.getElementById("productPrice");
+                    let productCategoryHTML = document.getElementById("productCategory");
+        
+                    productNameHTML.innerHTML = product.name;
+                    productDescriptionHTML.innerHTML = product.description;
+                    productSoldCountHTML.innerHTML = product.soldCount;
+                    productPriceHTML.innerHTML = product.currency + product.cost;
+        
+                    productCategoryHTML.innerHTML = product.category;
+        
+                    //Muestro las imagenes en forma de galería
+                    showProductImagesGallery(product.images);
+                }
+            });
+
+        }
+    })
+    
+});
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+ 
 });
 
