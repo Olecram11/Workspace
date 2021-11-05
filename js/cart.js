bc09ab615f876ad859c9778806580c821da59ffc
Olecram11 <marcelotejera11@gmail.com>
2021-11-05 00:26:13 -0300

    Comiteado carrito

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,6 @@
 var carrito;
+var subtotalDelCarrito;
+const TASA_DE_CAMBIO = 40;
 
 function mostrarProducto(){
     let html = ""
@@ -13,7 +15,7 @@ function mostrarProducto(){
             <h4 class="mb-1">${element.name}</h4>
             <input id="cantidad${i}" type="number" min="1" class="inputs" value="${element.count}" onchange="mostrarSubtotales(${i})"></input>
         </div>
-        <div class="mb-1">${element.currency}<span id="precio${i}">${element.unitCost}</span></div>
+        <div class="mb-1"><span id="moneda${i}">${element.currency}</span><span id="precio${i}">${element.unitCost}</span></div>
     </div>
     <div>Subtotal:</div>
     <div id=subtotal${i}>${element.count*element.unitCost}</div>
@@ -24,14 +26,64 @@ function mostrarProducto(){
     
 }
 
+function convertirMoneda(id, costo){
+let moneda = document.getElementById(`moneda${id}`).innerHTML;
+if (moneda === "USD"){
+ return costo * TASA_DE_CAMBIO;
+} else {
+ return costo
+}
+}
+
+function mostrarSubtotalCarrito(){
+    
+    subtotalDelCarrito = 0;
+    for (let i = 0; i < carrito.length; i++) {
+        subtotalDelCarrito += convertirMoneda(i, parseFloat(document.getElementById(`subtotal${i}`).innerHTML))
+    }
+    document.getElementById("productCostText").innerHTML=subtotalDelCarrito
+}
+
+function mostrarTotal(){
+
+    let totalCost = document.getElementById("totalCostText")
+    let inputs = document.getElementsByName("publicationType")
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked){
+            precioSubtotal = subtotalDelCarrito
+            var porcentajeDeEnvio = inputs[i].value
+            var mostrarTotal = parseFloat((Math.round(precioSubtotal*porcentajeDeEnvio/100)+precioSubtotal))
+            totalCost.innerHTML = mostrarTotal
+        }
+        
+    }
+    mostrarPrecioEnvio()
+}
+
+function mostrarPrecioEnvio(){
+
+    let totalCost = document.getElementById("comissionText")
+    let inputs = document.getElementsByName("publicationType")
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked){
+            precioSubtotal = subtotalDelCarrito
+            var porcentajeDeEnvio = inputs[i].value
+            var mostrarTotal = parseFloat((Math.round(precioSubtotal*(porcentajeDeEnvio/100))))
+            totalCost.innerHTML = mostrarTotal
+        }
+        
+    }
+
+}
+
 
 function mostrarSubtotales(i){
     
     let precio = parseFloat(document.getElementById(`precio${i}`).innerText); // trae el valor de precio unitario
     let cantidad = parseFloat(document.getElementById(`cantidad${i}`).value);
-    let subtotal = precio*cantidad
+    subtotal = precio*cantidad
 document.getElementById(`subtotal${i}`).innerHTML = subtotal    
-
+mostrarSubtotalCarrito();
 }
 
 
@@ -45,5 +97,6 @@ document.addEventListener("DOMContentLoaded", function(e){
     .then(function(resultObj2){
         carrito = resultObj2.articles
     mostrarProducto();
+    mostrarSubtotalCarrito();
     })
 });
